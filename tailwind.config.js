47a95d402d06fba5eb715bc8b18cb3aaf5eaa5fa
victorroddev <javier.rod.dev@gmail.com>
2024-06-18 22:01:42 -0600

    new commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,13 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     colors: {
-      
+      primaryblue: '3,61,88',
+      secondaryblue: '#2BA5B2',
+      primaryBlue75: '#2BA5B2 / 75%' ,
+      primaryyellow: '#F7AF02',
+      purpleprimary: '#3C4E76',
+      black: '#000000',
+      white: '#FFFFFF'
     },
     extend: {},
   },
