d455db90f7fbd3130667b8a9ad0cdce265374b92
victorroddev <javier.rod.dev@gmail.com>
2024-06-19 18:08:07 -0600

    Create Hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,31 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     colors: {
-      primaryblue: '3,61,88',
-      secondaryblue: '#2BA5B2',
-      primaryBlue75: '#2BA5B2 / 75%' ,
-      primaryyellow: '#F7AF02',
-      purpleprimary: '#3C4E76',
+      primaryBlue: '#033D58',
+      secondaryBlue: '#2BA5B2',
+      primaryBlue75: 'rgb(3 61 88 / 75%)',
+      secondaryBlue45: 'rgb(43 165 178 /45%)',
+      secondaryBlue0: 'rgb(43 165 178 / 0%)',
+      secondaryBlue25: 'rgb(43 165 178 /25%)',
+      primaryBlue20: 'rgb(3 61 88 / 20%)',
+      primaryYellow: '#F7AF02',
+      primaryPurple: '#3C4E76',
       black: '#000000',
       white: '#FFFFFF'
     },
-    extend: {},
+    extend: {
+      fontFamily: {
+        headers: ['Urbanist'],
+        paragraph: ['Public Sans']
+      },
+    rotate: {
+      '18': '18deg',
+      '22': '22deg',
+      '31': '31deg',
+      '45': '45deg'
+
+    }
+    },
   },
   plugins: [],
 }
